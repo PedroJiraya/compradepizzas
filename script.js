6c6e5465c9db08fb7cc60d3f7e8957364a78a90c
Pedro Pinheiro <pedroph12@gmail.com>
2025-05-16 14:25:16 -0300

    aula 4 concluida

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = (item.price).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
     pizzaItem.querySelector('.pizza-item--img img').setAttribute('src',`./${item.img}`)
+    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+        e.preventDefault()
+        
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1;
+            console.log('aplicado')
+        }, 100);
+    })
 
 
 // parou na aula 3 a proxima é a 4
