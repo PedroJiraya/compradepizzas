dbd8895b9d10c7b387c35b7817b342219197a3b4
Pedro Pinheiro <pedroph12@gmail.com>
2025-05-16 00:03:54 -0300

    aula 3 concluida

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ const cs = (el)=> document.querySelector(all)
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
-    c('.pizza-area').append
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = (item.price).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
+    pizzaItem.querySelector('.pizza-item--img img').setAttribute('src',`./${item.img}`)
+
+
+// parou na aula 3 a proxima é a 4
+
+
+    c('.pizza-area').append(pizzaItem)
 })                                                                                                                       
