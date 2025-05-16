0599430dfed5a5107dcacb99f9421d1088228916
Pedro Pinheiro <pedroph12@gmail.com>
2025-05-16 16:32:22 -0300

    etapa 5 ook

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const cs = (el)=> document.querySelector(all)
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
-
+    
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = (item.price).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
@@ -12,12 +13,19 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('a').addEventListener('click',(e)=>{
         e.preventDefault()
         
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+
+        c('.pizzaBig img').src = pizzaJson[key].img
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex'
         setTimeout(() => {
             c('.pizzaWindowArea').style.opacity = 1;
-            console.log('aplicado')
         }, 100);
+
+        
     })
 
 
