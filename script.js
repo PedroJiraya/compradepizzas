6f2513547de48a4edd7fbefd84d72b767c1a60cc
Pedro Pinheiro <pedroph12@gmail.com>
2025-05-15 22:56:18 -0300

    atualizando rep para mudar de pc

---
+++ script.js
@@ -0,0 +1,9 @@
+const c = (el)=> document.querySelector(el)
+const cs = (el)=> document.querySelector(all)
+
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    c('.pizza-area').append
+})                                                                                                                       
